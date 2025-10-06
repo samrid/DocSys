b3147314cb86bb9f9bc5a4a9d739dbdc8e01edaa
Puwanai Sangphon <puwanai.sangphon@gmail.com>
2025-10-06 13:27:02 +0700

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'System Admin',
+                'officer_id' => '9999',
+                'password' => Hash::make('admin1234'),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
